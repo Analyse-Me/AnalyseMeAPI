72c9c0963684acfcf34ef4b0ca3e0b8ccf98aff4
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-05 01:09:53 +0100

    Cleaning code

--- src/Models/WeatherForecast.cs
+++ src/Models/WeatherForecast.cs
@@ -1,15 +1,11 @@
 using System;
 
-namespace AnalyseMeAPI
+namespace AnalyseMeAPI.Models
 {
-    public class WeatherForecast
-    {
+    public class WeatherForecast {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
     }
 }
---
+++ src/Services/WeatherForecastService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using AnalyseMeAPI.Models;
+
+namespace AnalyseMeAPI.Services {
+    public class WeatherForecastService {
+        private string[] Summaries = new[] {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        
+        public IEnumerable<WeatherForecast> Get() {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast() {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray();
+        }
+    }
+}
