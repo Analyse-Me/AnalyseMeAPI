fc268cef2397303378fc18e4e6aaa0249a728e85
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-19 19:59:12 +0100

    Now Everything works!

--- src/Models/Quiz.cs
+++ src/Models/Quiz.cs
@@ -1,8 +1,8 @@
 namespace AnalyseMeAPI.Models {
     public class _Compass {
-        int econ { get; set; }
-        int govt { get; set; }
-        int scty { get; set; }
+        public int econ { get; set; }
+        public int govt { get; set; }
+        public int scty { get; set; }
     }
     public class _8Values {
         public int econ { get; set; }
@@ -11,30 +11,30 @@ namespace AnalyseMeAPI.Models {
         public int scty { get; set; }
     }
     public class _9Axes {
-        int fed { get; set; }
-        int dem { get; set; }
-        int glo { get; set; }
-        int mil { get; set; }
-        int fre { get; set; }
-        int equ { get; set; }
-        int sec { get; set; }
-        int pro { get; set; }
-        int mul { get; set; }
+        public int fed { get; set; }
+        public int dem { get; set; }
+        public int glo { get; set; }
+        public int mil { get; set; }
+        public int fre { get; set; }
+        public int equ { get; set; }
+        public int sec { get; set; }
+        public int pro { get; set; }
+        public int mul { get; set; }
     }
     public class _Soulgraphy_PI {
-        int econ_soci { get; set; }
-        int econ_regu { get; set; }
-        int econ_prot { get; set; }
-        int state_glob { get; set; }
-        int state_peac { get; set; }
-        int state_fede { get; set; }
-        int state_demo { get; set; }
-        int society_ind { get; set; }
-        int society_prog { get; set; }
-        int society_secu { get; set; }
-        int society_revo { get; set; }
-        int law_free { get; set; }
-        int law_reso { get; set; }
-        int law_ecol { get; set; }
+        public int econ_soci { get; set; }
+        public int econ_regu { get; set; }
+        public int econ_prot { get; set; }
+        public int state_glob { get; set; }
+        public int state_peac { get; set; }
+        public int state_fede { get; set; }
+        public int state_demo { get; set; }
+        public int society_ind { get; set; }
+        public int society_prog { get; set; }
+        public int society_secu { get; set; }
+        public int society_revo { get; set; }
+        public int law_free { get; set; }
+        public int law_reso { get; set; }
+        public int law_ecol { get; set; }
     }
 }
